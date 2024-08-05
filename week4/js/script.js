d5c31948c7cceadba078c4dac20a313aaa3e8fce
Abner Jessé <abnerjesse@gmail.com>
2024-08-05 17:30:41 -0400

    feat: started script.js from lista de compras

--- week4/js/script.js
+++ week4/js/script.js
@@ -1,1 +1,10 @@
+let index = 0
+let listaCompras = []
 
+function adicionarItem() {
+    let item = document.getElementById("item").value
+    listaCompras[index] = item
+    index ++
+    document.getElementById("item").value = ""
+    document.getElementById("lista").innerText = listaCompras
+}
