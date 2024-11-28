500ec97f6297791dd84628c9a9e238602bf63ffc
Abner Jessé <abnerjesse@gmail.com>
2024-11-28 16:30:17 -0400

    repeticao ex1 added

---
+++ exercises1/repeticao/ex1/inverted-number.js
@@ -0,0 +1,41 @@
+// let list = []
+
+// function printList() {
+//     let num = parseInt(document.getElementById("number").value)
+//     document.getElementById("showList").innerHTML = ''
+
+//     if (Number.isInteger(num) && num > 0) {
+//         let numLoop = num
+//         for (let index = 0; index < num; index++) {
+//             list.push(numLoop)
+//             numLoop--
+//         }
+
+//         document.getElementById("showList").innerHTML = `<p>${list.join(", ")}</p>`
+
+//     } else {
+//         alert('Digite um número inteiro maior que zero!')
+//         document.getElementById("number").value = ''
+//         document.getElementById("showList").innerHTML = ''
+//     }
+// }
+
+// document.getElementById("btn").onclick = printList
+
+function generateInvertedList() {
+    const numInput = document.getElementById("number");
+    const outputContainer = document.getElementById("showList");
+    outputContainer.innerHTML = "";
+
+    const num = parseInt(numInput.value);
+
+    if (Number.isInteger(num) && num > 0) {
+        const list = Array.from({ length: num }, (_, i) => num - i);
+        outputContainer.innerHTML = `<p>${list.join(", ")}</p>`;
+    } else {
+        alert("Digite um número inteiro maior que zero!");
+        numInput.value = "";
+    }
+}
+
+document.getElementById("btn").addEventListener("click", generateInvertedList);
