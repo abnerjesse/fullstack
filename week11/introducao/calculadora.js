505e46d86d1df7b6e5278c0da9dd77cc013c34e5
Abner Jessé <abnerjesse@gmail.com>
2024-12-26 13:25:28 -0400

    week11, class 2

---
+++ week11/introducao/calculadora.js
@@ -0,0 +1,24 @@
+function add(n1, n2) {
+    return n1 + n2
+}
+
+function sub(n1, n2) {
+    return n1 - n2
+}
+
+function mul(n1, n2) {
+    return n1 * n2
+}
+
+function div(n1, n2) {
+    if (n2 > 0) return n1 / n2
+    return null
+}
+
+console.log("O resultado da some é " + add(2, 3) + "!");
+console.log(`O resultado da subtração é ${sub(2, 3)}!`);
+console.log(`O resultado da multiplicação é ${mul(2, 3)}!`);
+
+let divResult = div(20, 0)
+if (divResult != null) console.log(`O resultado da divisão é ${divResult}!`)
+else console.log(`O divisor deve ser maior que zero!`)
