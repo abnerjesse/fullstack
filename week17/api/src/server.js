f540a6e187a00a9e20763fac1905e48dcb3c98f9
Abner Jessé <abnerjesse@gmail.com>
2025-05-01 17:19:35 -0400

    week17 feat api with MongoDB

---
+++ week17/api/src/server.js
@@ -0,0 +1,22 @@
+const db = require('mongoose')
+db.connect('mongodb+srv://abnerjesse:<db_password>@ajdbhightech.ktmclvd.mongodb.net/?retryWrites=true&w=majority&appName=ajdbhightech')
+
+const perfilRoute = require('./routes/PerfilRoute')
+const notificacaoRoute = require('./routes/NotificacaoRoute')
+const loginRoute = require('./routes/LoginRoute')
+
+const express = require('express')
+const api = express()
+api.use(express.json())
+
+api.use('/perfil', perfilRoute)
+api.use('/notificacao', notificacaoRoute)
+api.use('/login', loginRoute)
+
+
+//Rota raiz
+api.get('/', (req, res) => {
+    res.send('Bem-vindo(a) ao Perfil Profissional API!')
+})
+
+module.exports = api
