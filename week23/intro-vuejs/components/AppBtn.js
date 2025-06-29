903b14561751e96bd88090309c56b535d811d2d3
Abner Jessé <abnerjesse@gmail.com>
2025-06-29 15:50:00 -0400

    week23 vuejs intro components

---
+++ week23/intro-vuejs/components/AppBtn.js
@@ -0,0 +1,10 @@
+export default {
+    data: function () {
+        return {
+            title: "Olá",
+        };
+    },
+    template: `
+    <button style="backgroud-color: #2c3e50; color: black; font-family: Arial, Helvetica, sans-serif; border: 1px solid black; padding: 10px; border-radius: 5px;">{{ title }}</button>
+    `
+};
