40e9ca07c1fad66cbc244de927d27f6f78703c27
Abner Jessé <abnerjesse@gmail.com>
2025-07-02 00:50:30 -0400

    api to heroku

---
+++ perfil-profissional-api/src/services/LoginService.js
@@ -0,0 +1,45 @@
+const perfilModel = require('../models/Perfil')
+const tokenUtil = require('../utils/TokenUtil')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+    autenticar: async (usuario) => {
+        try {
+            if (usuario.email && usuario.senha) {
+                let perfilEncontrado = await perfilModel.findOne({
+                    "usuario.email": usuario.email
+                }).select('+usuario.senha').exec()
+
+                if (perfilEncontrado) {
+                    const match = await bcrypt.compare(usuario.senha, perfilEncontrado.usuario.senha)
+
+                    if (match) {
+                        const token = tokenUtil.gerarToken(JSON.stringify(perfilEncontrado.usuario))
+                        return ({
+                            token: token,
+                            email: perfilEncontrado.usuario.email,
+                            id: perfilEncontrado._id
+                        })
+                    } else {
+                        throw {
+                            status: 200,
+                            message: "Erro ao efetuar o login. Usuário ou senha incorretos!"
+                        }
+                    }
+                } else {
+                    throw {
+                        status: 200,
+                        message: "Erro ao efetuar o login. Usuário ou senha incorretos!"
+                    }
+                }
+            } else {
+                throw {
+                    status: 400,
+                    message: "Erro ao efetuar o login. Dados incompletos!"
+                }
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+}
---
+++ perfil-profissional-api/src/services/NotificacaoService.js
@@ -0,0 +1,42 @@
+const notificacaoModel = require('../models/Notificacao')
+
+module.exports = {
+    buscarPorId: async (id) => {
+        try {
+            return await notificacaoModel.findOne({ _id: id })
+        } catch (error) {
+            throw { message: error.message, status: 500 }
+        }
+    },
+
+    buscarPorPerfilId: async (perfilID) => {
+        try {
+            return await notificacaoModel.find({ $or: [{ remetente: perfilID }, { destinatario: perfilID }] })
+        } catch (error) {
+            throw { message: error.message, status: 500 }
+        }
+    },
+
+    cadastrar: async (notificacao) => {
+        try {
+            return await notificacaoModel.create(notificacao)
+        } catch (error) {
+            throw { message: error.message, status: 500 }
+        }
+    },
+
+    marcarLida: async (id) => {
+        try {
+            let notificacao = await notificacaoModel.findOne({ _id: id })
+
+            if (!notificacao) {
+                throw { message: "Notificação inexistente!", status: 404 }
+            }
+            notificacao.lida = true
+            await notificacao.save()
+            return { message: "Notificação lida!", status: 200 }
+        } catch (error) {
+            throw { message: error.message, status: 500 }
+        }
+    }
+}
